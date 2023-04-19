9c11a3ae3225a1cbc69f9b1266e8a7d414479d8a
buchii <business.buchii@gmail.com>
2023-04-19 09:28:43 +0100

    C - variables, I/O

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask for the user's first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask for  the user's last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        
+        // Print the given name to the terminal
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
