25466b3f48e93322b5c3b0e48299af5da9875ff6
buchii <business.buchii@gmail.com>
2023-04-28 22:07:37 +0100

    C# - Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int favNum = PromptUserNumber();
+
+        int sqrRoot = SquareNumber(favNum);
+
+        DisplayResult(userName, sqrRoot);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNum = int.Parse(Console.ReadLine());
+
+        return favNum;
+    }
+
+    static int SquareNumber(int num)
+    {
+        int square = num * num;
+
+        return square;
+    }
+
+    static void DisplayResult(string userName, int sqrNum)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {sqrNum}");
     }
 }
