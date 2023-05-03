452236b6f4704260609348f0e75ff1687d655188
buchii <business.buchii@gmail.com>
2023-05-03 16:04:05 +0100

    Process  the Resume and Job class in main

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create an instance of the Job class named job1
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Create another instance of the Job class named job2
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create an instance of the Resume class named resume1
+        Resume resume1 = new Resume();
+        resume1._name = "Allison Rose";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        // Display job details
+        resume1.DisplayResumeDetails();
+
+
+
     }
 }
