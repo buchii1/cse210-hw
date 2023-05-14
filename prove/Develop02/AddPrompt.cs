f6ef88555121eb2e915863f9724ac6c24ee21f94
buchii <business.buchii@gmail.com>
2023-05-14 02:57:34 +0100

    Added ability for users to add new prompts

---
+++ prove/Develop02/AddPrompt.cs
@@ -0,0 +1,10 @@
+public class AddPrompt
+{
+    public string _prompt;
+
+    public void AddNewPrompt()
+    {
+        Console.WriteLine("Add a new question prompt:");
+        _prompt = Console.ReadLine();
+    }
+}
