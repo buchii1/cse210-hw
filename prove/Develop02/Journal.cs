d966e53578387ff4c6554358f16bcae3355b0a58
buchii <business.buchii@gmail.com>
2023-05-14 02:58:34 +0100

    A list to store journal entries

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+public class Journal
+{
+    public List<Entry> _journal = new List<Entry>();
+
+    public void DisplayJournal()
+    {
+        foreach (Entry entry in _journal)
+        {
+            entry.DisplayEntry();
+        }
+    }
+}
