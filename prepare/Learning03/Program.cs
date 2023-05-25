7cf8521358ba8bdb5d8e42e6a8638131d827b1e8
buchii <business.buchii@gmail.com>
2023-05-26 00:24:45 +0100

    Practicing the principles of Encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Instantiate an instance of the obj
+        Fraction fraction_3 = new Fraction();
+
+        fraction_3.SetTop(3);
+        fraction_3.SetBottom(4);
+
+        Console.WriteLine(fraction_3.GetTop());
+        Console.WriteLine(fraction_3.GetBottom());
+        Console.WriteLine(fraction_3.GetFractionString());
+        Console.WriteLine(fraction_3.GetDecimalValue());
     }
 }
