9c5476ddfb6f0b8ee32465ebfa8de9865c388114
buchii <business.buchii@gmail.com>
2023-05-28 06:03:02 +0100

    Complete Implementation of the Word Class

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,26 @@
+public class Word
+{
+    private string _word;
+    private bool _isHidden;
+
+    public Word(string word)
+    {
+        _word = word;
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public void HideWord()
+    {
+       _isHidden = true; 
+    }
+
+    public string GetWord()
+    {
+        return _word;
+    }
+}
