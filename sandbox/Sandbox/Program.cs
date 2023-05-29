50102f14bb8cbd6a332c6285d6051c3d09da2661
buchii <business.buchii@gmail.com>
2023-05-29 17:40:14 +0100

    Practice file

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,48 +1,11 @@
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-    }
-}
-
-public class Reference
-{
-    private string _book;
-    private string _chapter;
-    private string _verse;
-    private string _endVerse;
-
-    public Reference(string book, string chapter, string verse)
-    {
-        _book = book;
-        _chapter = chapter;
-        _verse = verse;
-    }
-
-    public Reference(string book, string chapter, string verse, string endVerse)
-    {
-        _book = book;
-        _chapter = chapter;
-        _verse = verse;
-        _endVerse = endVerse;
-    }
-
-    // A method that displays the reference
-    public string GetReference() {
-       string reference;
-
-       if (_endVerse != null)
-       {
-            reference = $"{_book} {_chapter}:{_verse}-{_endVerse}";
-       }
-       else
-       {
-            reference = $"{_book} {_chapter}:{_verse}";
-       }
-
-       return reference;
+        Console.WriteLine("Sandbox");
     }
 }
