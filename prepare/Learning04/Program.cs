f415927c3156fae34423a9a89f7dd899efac49c5
buchii <business.buchii@gmail.com>
2023-06-01 18:07:13 +0100

    Added functionality to program main class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment newA = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-9");
+        Console.WriteLine(newA.GetSummary());
+        Console.WriteLine(newA.GetHomeworkList());
+        
+        Console.WriteLine();
+
+        WritingAssignment newW = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(newW.GetSummary());
+        Console.WriteLine(newW.GetWritingInformation());
     }
 }
