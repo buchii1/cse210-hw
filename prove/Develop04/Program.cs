8634a939aa1e20abe985a8f2affd5a43bc61bfa0
buchii1 <oko22007@byui.edu>
2023-06-11 04:39:05 +0100

    Added main program class

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int userInput;
+        
+        do
+        {
+            Menu menu = new Menu();
+            menu.DisplayMenu();
+            userInput = int.Parse(Console.ReadLine());
+            if (userInput == 1)
+            {
+                Breathing breath = new Breathing();
+                breath.RunActivity();
+            }
+            else if (userInput == 2)
+            {
+                Reflecting reflect = new Reflecting();
+                reflect.RunActivity();
+            }
+            else if (userInput == 3)
+            {
+                Listing list = new Listing();
+                list.RunActivity();
+            }
+            else
+            {
+                Console.WriteLine("Thank you for participating in this exercise.");
+            }
+        } while (userInput < 4);
     }
 }
+
+/*
+EXTRA REQUIREMENTS
+
+1. I added a codeblock that prevents duplicate prompts from being displayed in the Reflection and Listing Activity.
+
+*/
