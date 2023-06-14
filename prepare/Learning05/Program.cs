a7f2c5343a111597f4a5dfc52e1a046756912346
buchii1 <oko22007@byui.edu>
2023-06-14 23:03:11 +0100

    Main class for the Area of Shapes program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square sqr = new Square("Green", 4);
+        Rectangle rec = new Rectangle("Yellow", 3, 9);
+        Circle circle = new Circle("White", 7);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(sqr);
+        shapes.Add(rec);
+        shapes.Add(circle);
+
+        foreach (Shape obj in shapes)
+        {
+            Console.WriteLine($"The area of the {obj.Color} shape is {obj.GetArea()}.");
+        }
     }
 }
