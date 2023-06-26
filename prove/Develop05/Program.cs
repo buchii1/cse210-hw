f80b45a4c7c4ddf41e505138a0d3bfd6b4e805cc
buchii1 <oko22007@byui.edu>
2023-06-26 03:55:00 +0100

    Main program implementation

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        Menu menu = new Menu();
+        SimpleGoal simpleGoal = new SimpleGoal();
+        EternalGoal eternalGoal = new EternalGoal();
+        ChecklistGoal checklistGoal = new ChecklistGoal();
+        GoalTracker trackGoal = new GoalTracker();
+
+        int userInput;
+
+        do
+        {
+            Console.WriteLine($"\nYou have {trackGoal.GetTotalScore()} points.\n");
+
+            menu.DisplayMenu();
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == 1)
+            {
+                menu.DisplayGoalTypes();
+                int prompt = int.Parse(Console.ReadLine());
+
+                if (prompt == 1)
+                {
+                    simpleGoal.DisplayStartMessage();
+                    trackGoal.AddGoal(simpleGoal);
+                }
+                else if (prompt == 2)
+                {
+                    eternalGoal.DisplayStartMessage();
+                    trackGoal.AddGoal(eternalGoal);
+                }
+                else
+                {
+                    checklistGoal.DisplayStartMessage();
+                    trackGoal.AddGoal(checklistGoal);
+                }
+            }
+            else if (userInput == 2)
+            {
+                trackGoal.DisplayGoals();
+            }
+            else if (userInput == 3)
+            {
+                Console.Write("What is the filename for the goal file? ");
+                string fileName = Console.ReadLine();
+                trackGoal.SaveGoals(fileName);
+            }
+            else if (userInput == 4)
+            {
+                Console.Write("What is the filename for the goal file? ");
+                string fileName = Console.ReadLine();
+                trackGoal.LoadGoals(fileName);
+            }
+            else if (userInput == 5)
+            {
+                trackGoal.RecordGoal();
+            }
+            else
+            {
+                Console.WriteLine("Thanks for using our app!");
+            }
+        } while (userInput < 6);
     }
 }
